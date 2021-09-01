9775c0fb9943a5ec1136c5ffb53d849c013e8d37
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-09-01 12:48:55 -0300

    realizando la discografia

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+
+
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems,{
       duration: 600,
