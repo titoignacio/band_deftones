75101b324d204a97253ba8cd913b1548491286fb
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-09-02 15:12:16 -0300

    nav responsive listo

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
 
