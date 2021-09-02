1d81b89a90e4a752d862e9b917d01511e909dac5
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-09-02 17:21:27 -0300

    colocando scroll reveal js

--- main.js
+++ main.js
@@ -14,3 +14,88 @@ document.addEventListener('DOMContentLoaded', function() {
       
     });
   });
+
+
+  /* Scroll Reveal */
+
+  window.sr = ScrollReveal();
+    sr.reveal('.navi', {
+    duration: 2000,
+    origin: 'bottom'
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.carousel', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.prueba', {
+      duration: 2000,
+      origin: 'right',
+      distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.card', {
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.layout', {
+      duration: 2000,
+      origin: 'top',
+      distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.fooot', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '300px',
+    });
+
+    /* discografia */
+
+    window.sr = ScrollReveal();
+    sr.reveal('.float', {
+      duration: 2000,
+      origin: 'bottom',
+      distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.flip', {
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+    });
+
+    /* video */
+
+    window.sr = ScrollReveal();
+    sr.reveal('.video', {
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.subtitulos', {
+      duration: 2000,
+      origin: 'top',
+      distance: '300px',
+    });
+
+    /* store */
+
+    window.sr = ScrollReveal();
+    sr.reveal('.card-image', {
+      duration: 2000,
+      origin: 'top',
+      distance: '300px',
+    });
